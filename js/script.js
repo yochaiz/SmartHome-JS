7b228c4e9b962de2bd541b961cce31ec3d2f8d23
yochaiz <yochaizur@gmail.com>
2016-11-08 20:21:29 +0200

    Reading XML file

--- js/script.js
+++ js/script.js
@@ -2,5 +2,16 @@
  * Created by yochaiz on 08-Nov-16.
  */
 
-$("#someElement3").append( "aer2u" );
+$("#someElement3").append("aer2u");
 
+$(document).ready(function () { // load xml file using jquery ajax
+    $.ajax({
+        type: "GET",
+        url: "../data/Devices.LightsAndAutomation.LightPoint.1.2.xml",
+        dataType: "xml",
+        success: function (xml) {
+            var $OnOff = $(xml).find("OnOff");
+            $("#someElement").append($OnOff.length);
+        }
+    });
+});
