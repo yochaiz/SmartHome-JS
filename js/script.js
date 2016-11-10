b5a006727801008cae551fb4ef5c8ed69be38411
yochaiz <yochaizur@gmail.com>
2016-11-10 13:37:50 +0200

    Loop function

--- js/script.js
+++ js/script.js
@@ -4,14 +4,42 @@
 
 $("#someElement3").append("aer2u");
 
-$(document).ready(function () { // load xml file using jquery ajax
+function loop(circle, elem, OnOff, i, max, time) {
+    if (i < max) {
+        var val = OnOff[i].textContent;
+        if (val == "null") {
+            circle.css({fill: "#ffffff"}); // white
+        } else if (val == "0") {
+            circle.css({fill: "#FF0000"}); // red
+        } else {
+            circle.css({fill: "#28B463"}); // green
+        }
+        elem.append('[');
+        elem.append(val);
+        elem.append(']');
+
+        i++;
+        setTimeout(loop.bind(null, circle, elem, OnOff, i, max, time), time)
+    }
+}
+
+function simulate() {
     $.ajax({
         type: "GET",
         url: "../data/Devices.LightsAndAutomation.LightPoint.1.2.xml",
         dataType: "xml",
         success: function (xml) {
             var $OnOff = $(xml).find("OnOff");
-            $("#someElement").append($OnOff.length);
+            var elem = $("#someElement");
+            elem.append('[');
+            elem.append($OnOff.length);
+            elem.append(']');
+            var circle = $('circle');
+            var time = 1000;
+            if (circle != null) {
+                var numOfIter = Math.min(10, $OnOff.length);
+                loop(circle, elem, $OnOff, 0, numOfIter, time);
+            }
         }
     });
-});
+}
