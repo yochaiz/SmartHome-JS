04944ab579bd3a8aa124db7523b3afe51549c9de
yochaiz <yochaizur@gmail.com>
2016-11-16 12:46:56 +0200

    Light Class

---
+++ js/Light.js
@@ -0,0 +1,84 @@
+/**
+ * Created by yochaiz on 16-Nov-16.
+ */
+
+function Light(element, filename) {
+    this.filename = this.getFileName(filename);
+    this.image = $(element).find('img');
+    this.OnOff = null;
+}
+
+Light.time = 200; // ms
+Light.fileNamePrefix = "../data/Devices.LightsAndAutomation.LightPoint.";
+Light.srcLightOff = '../icons/LightOff.png';
+Light.srcLightOn = '../icons/LightOn.png';
+Light.srcQuestionMark = '../icons/QuestionMark.png';
+
+Light.prototype.getFileName = function (fname) {
+    //var filename = this.element.getAttribute(attrName);
+    var url = Light.fileNamePrefix;
+    url += fname;
+    url += ".xml";
+
+    return url;
+};
+
+Light.parseTime = function (str) {
+    var idx = str.indexOf(' ');
+    date = null;
+    time = null;
+
+    if (idx >= 0) {
+        var date = str.substring(0, idx);
+        var time = str.substring(idx + 1, str.length);
+    }
+
+    return {date: date, time: time};
+};
+
+Light.prototype.visualize = function (elem, i, max) {
+    if (i < max) {
+        var el = this.OnOff[i];
+        var atts = el.attributes;
+        $.each(atts, function (idx, value) {
+            Light.parseTime(value.nodeValue);
+        });
+
+        var val = el.textContent;
+        var imgSrc = Light.srcQuestionMark;
+        if (val == "0") {
+            imgSrc = Light.srcLightOff;
+        } else if (val == "1") {
+            imgSrc = Light.srcLightOn;
+        }
+        this.image.attr('src', imgSrc);
+
+        elem.append('[');
+        elem.append(val);
+        elem.append(']');
+
+        i++;
+        setTimeout(this.visualize.bind(this, elem, i, max), Light.time);
+    }
+};
+
+Light.prototype.simulate = function () {
+    var light = this;
+    $.ajax({
+        type: "GET",
+        url: this.filename,
+        dataType: "xml",
+        success: function (xml) {
+            light.OnOff = $(xml).find("OnOff");
+            var elem = $("#someElement");
+            elem.append('[');
+            elem.append(light.OnOff.length);
+            elem.append(']');
+            if (light.image != null) {
+                var numOfIter = Math.min(50, light.OnOff.length);
+                // loop(img, elem, $OnOff, 0, numOfIter, time);
+                light.visualize(elem, 0, numOfIter);
+            }
+        }
+    });
+};
--- js/script.js
+++ js/script.js
@@ -4,75 +4,12 @@
 
 $("#someElement3").append("aer2u");
 
-function parseTime(str) {
-    var idx = str.indexOf(' ');
-    date = null;
-    time = null;
-    if (idx >= 0) {
-        var date = str.substring(0, idx);
-        var time = str.substring(idx + 1, str.length);
-    }
-
-    return {date: date, time: time};
-}
-
-function loop(circle, elem, OnOff, i, max, time) {
-    if (i < max) {
-        var el = OnOff[i];
-        var atts = el.attributes;
-        $.each(atts, function (idx, value) {
-            var content = parseTime(value.nodeValue);
-        });
-
-        var val = OnOff[i].textContent;
-        if (val == "null") {
-            circle.attr('src', '../icons/QuestionMark.png');
-        } else if (val == "0") {
-            circle.attr('src', '../icons/LightOff.png');
-        } else {
-            circle.attr('src', '../icons/LightOn.png');
-        }
-        elem.append('[');
-        elem.append(val);
-        elem.append(']');
-
-        i++;
-        setTimeout(loop.bind(null, circle, elem, OnOff, i, max, time), time)
-    }
-}
-
 var attrName = 'data-file';
-
-function getFileName(element) {
-    var filename = element.getAttribute(attrName);
-    var url = "../data/Devices.LightsAndAutomation.LightPoint.";
-    url += filename;
-    url += ".xml";
-
-    return url;
-}
 
 function simulate() {
     var td = $("td[" + attrName + "]");
     $.each(td, function (idx, element) {
-        var filename = getFileName(element);
-        $.ajax({
-            type: "GET",
-            url: filename,
-            dataType: "xml",
-            success: function (xml) {
-                var $OnOff = $(xml).find("OnOff");
-                var elem = $("#someElement");
-                elem.append('[');
-                elem.append($OnOff.length);
-                elem.append(']');
-                var img = $(element).find('img');
-                if (img != null) {
-                    var time = 200;
-                    var numOfIter = Math.min(50, $OnOff.length);
-                    loop(img, elem, $OnOff, 0, numOfIter, time);
-                }
-            }
-        });
+        var light = new Light(element, element.getAttribute(attrName));
+        light.simulate();
     });
 }
